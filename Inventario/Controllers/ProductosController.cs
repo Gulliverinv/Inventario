bba3d0d285bbb2b33a6af9eb482950c43693dc20
Gulliverinv <jorge.curiman@gmail.com>
2023-08-29 13:23:55 -0400

    MVC C#

---
+++ Inventario/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using Inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Reporte()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Producto producto)
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventario/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Color { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+        public DateTime FechaCreacion { get; private set; }
+
+        public Producto()
+        {
+            FechaCreacion = DateTime.Now;
+        }
+    }
+}
